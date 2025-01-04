cb4469aa6355e65e1c14b06ea1dd6d5b574ce1e2
SolixHub NPC <140714711+debunked69@users.noreply.github.com>
2025-01-04 12:31:14 +0100

    Add files via upload

---
+++ solix v2 new loader.lua
@@ -0,0 +1,21 @@
+
+
+if game.CreatorId == 4372130 then --Blox fruits 
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e2718ddebf562c5c4080dfce26b09398.lua"))()
+elseif game.CreatorId == 4294803 then -- Demonfall 
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b64d07193c7c2aef970d57aeb286e70.lua"))()
+elseif game.CreatorId == 1226491 then -- Shindo Life
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fefdf5088c44beb34ef52ed6b520507c.lua"))()
+elseif game.CreatorId == 2613928 then -- Counter Blox
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0e0166cb8f3c5c189c6a1f2532db2125.lua"))()
+elseif game.CreatorId == 3229308 then -- Grand Piece Online
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4b2841eb3e1345dd1aa50da164ca2bbe.lua"))()
+elseif game.CreatorId == 7381705 then -- Fisch
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8bbc8a7c4e023bc0c80799fec3233162.lua"))()
+elseif game.CreatorId == 15557357 then -- Jujutsu Infinite
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4fe2dfc202115670b1813277df916ab2.lua"))()
+elseif game.CreatorId == 1848960 then -- Murder Mystery 2
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9c916252a9f79bbf5a108f97c531e807.lua"))()
+elseif game.CreatorId == 3959677 then -- Pet Simulator 99
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e95ef6f27596e636a7d706375c040de4.lua"))()
+end
