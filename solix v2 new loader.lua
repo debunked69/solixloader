ce6c89bd47945083b0f2a6eec86bcd9993bf7730
SolixHub NPC <140714711+debunked69@users.noreply.github.com>
2025-04-24 23:01:01 +0200

    Update solix v2 new loader.lua

--- solix v2 new loader.lua
+++ solix v2 new loader.lua
@@ -1,11 +1,7 @@
 repeat wait() until game:IsLoaded()
 
 if game.CreatorId == 4372130 then --Blox fruits 
-    if hookmetamethod and require then
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e2718ddebf562c5c4080dfce26b09398.lua"))()
-    else
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f1d09e6c5ebfe2ed922a61c4bfba05d3.lua"))()
-    end
+   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e2718ddebf562c5c4080dfce26b09398.lua"))()
 elseif game.CreatorId == 11867394 then -- Dead Rails
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/98f5c64a0a9ecca29517078597bbcbdb.lua"))()
 elseif game.CreatorId == 4294803 then -- Demonfall 
